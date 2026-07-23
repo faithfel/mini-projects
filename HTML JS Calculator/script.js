c3d06f92d0233a53f99c35b39da0a0549af08d4c
Faith <felominofaith@gmail.com>
2026-07-23 13:45:18 +0800

    calc

---
+++ HTML JS Calculator/script.js
@@ -0,0 +1,35 @@
+function calc () {
+const num1 = document.querySelector('.first');
+const num2 = document.querySelector('.second');
+
+const op = document.querySelector('.op');
+
+const container = document.querySelector('.result');
+
+let a = parseInt(num1.value);
+let b = parseInt(num2.value);
+let operator = op.value;
+
+if (operator === "+") {
+   let answer = a + b;
+   container.textContent = answer;
+}
+else if (operator === "-") {
+   let answer = a - b;
+   container.textContent = answer;
+}
+else if (operator === "*") {
+   let answer = a * b;
+   container.textContent = answer;
+}
+else if (operator === "/") {
+   let answer = a / b;
+   container.textContent = answer;
+}
+else if (operator === "%") {
+   let answer = a % b;
+   container.textContent = answer;
+}
+
+}
+
