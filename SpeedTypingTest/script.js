973c0be3e1bf55208420cc8ebd4699b038d47925
Faith <felominofaith@gmail.com>
2026-07-30 10:27:33 +0800

    e

--- SpeedTypingTest/script.js
+++ SpeedTypingTest/script.js
@@ -1,1 +1,6 @@
-const typedByUser = document.querySelector('#typingInput');
+const typedByUser = document.querySelector('#typingInput').value;
+let typedLength = typedByUser.length;
+
+while (typedLength < 0) {
+    console.log('hi')
+}
