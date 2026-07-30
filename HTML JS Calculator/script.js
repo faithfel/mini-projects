5a2676049c36634f4901c6eb51fb6f1a1091e6e6
Faith <felominofaith@gmail.com>
2026-07-30 10:21:45 +0800

    first commit (typing test)

--- HTML JS Calculator/script.js
+++ HTML JS Calculator/script.js
@@ -3,32 +3,27 @@ const num1 = document.querySelector('.first');
 const num2 = document.querySelector('.second');
 
 const op = document.querySelector('.op');
-
 const container = document.querySelector('h3');
 
 let a = parseInt(num1.value);
 let b = parseInt(num2.value);
 let operator = op.value;
 
-if (operator === "+") {
-   let answer = a + b;
-   container.textContent = answer;
+let answer;
+
+   answer = a + b;
 }
 else if (operator === "-") {
-   let answer = a - b;
-   container.textContent = answer;
 }
 else if (operator === "*") {
-   let answer = a * b;
-   container.textContent = answer;
+   answer = a * b;
 }
 else if (operator === "/") {
-   let answer = a / b;
-   container.textContent = answer;
+   answer = a / b;
 }
 else if (operator === "%") {
-   let answer = a % b;
-   container.textContent = answer;
+   answer = a % b;
 }
+container.textContent = answer;
 }
 
---
+++ SpeedTypingTest/script.js
@@ -0,0 +1,1 @@
+const typedByUser = document.querySelector('#typingInput');
