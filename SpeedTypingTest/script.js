0e799f2af504dee3918bbbc37f1692c7ec6e6c55
Faith <felominofaith@gmail.com>
2026-07-30 10:45:53 +0800

    api

--- SpeedTypingTest/script.js
+++ SpeedTypingTest/script.js
@@ -1,6 +1,22 @@
-const typedByUser = document.querySelector('#typingInput').value;
-let typedLength = typedByUser.length;
+const typedByUser = document.querySelector('#typingInput');
 
-while (typedLength < 0) {
-    console.log('hi')
-}
+//getting every input from user 
+typedByUser.addEventListener('input', (event) => {
+    console.log(event.target.value); 
+});
+
+// never remove focus out of input
+typedByUser.addEventListener('blur', () => {
+    typedByUser.focus();
+});
+
+async function getRandomWord() {
+
+    // Fetches 1 random word from the public API
+    const response = await fetch('https://random-word-api.herokuapp.com/all');
+    const data = await response.json();
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomItem = data[randomIndex];
+    console.log("Your random selection:", randomItem);
+    return randomItem;
+  }
